c8ee415a154a01adf3ce695f64cf8b40af747944
CyberAakash <aakashvnth@gmail.com>
2023-10-04 00:03:39 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Navb from './components/Navbar';
+import Head from './components/Header';
+import Hero from './components/Hero';
+import Foot from './components/Footer';
+import Cont from './components/Contact';
+
+const App = () => {
+  return (
+    <div>
+      
+      <Navb />
+      <Head/>
+      <Hero/>
+      <Cont/>
+      
+      
+    
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Model.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Model = () => {
+  return (
+    <div>
+      
+      <h1 className='text-white ml-10 pt-20'>Sriram S</h1>
+    </div>
+  )
+}
+
+export default Model
